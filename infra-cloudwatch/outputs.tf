e14c0fdb3b2cf50b9b25710735458c5df7349dda
noraa <noab004@egms.no>
2025-11-19 22:44:07 +0100

    oppgave 4b ferdig?

---
+++ infra-cloudwatch/outputs.tf
@@ -0,0 +1,6 @@
+output "sns_topic_arn" {
+    value = aws_sns_topic.alerts.arn
+    }
+output "dashboard_name" {
+    value = aws_cloudwatch_dashboard.app_dashboard.dashboard_name
+  }
---
+++ infra-cloudwatch/variables.tf
@@ -0,0 +1,4 @@
+variable "alert_email" {
+        type = string
+        description = "Email for receiving alerts"
+    }
