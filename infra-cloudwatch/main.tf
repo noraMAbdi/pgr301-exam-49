752b11676a4d5d9ea406bbaae0b8ca8391414c4d
noraa <noab004@egms.no>
2025-11-19 22:43:38 +0100

    main.tf fikset kandiatnr

--- infra-cloudwatch/main.tf
+++ infra-cloudwatch/main.tf
@@ -10,50 +10,56 @@ resource "aws_sns_topic_subscription" "email_sub"{
     protocol = "email"
     endpoint = var.alert_email
   }
-resource "aws_cloudwatch_metric_alarm" "latency_alarm"{
-    alarm_name = "HighLatency"
+resource "aws_cloudwatch_metric_alarm" "companies_detected_high"{
+    alarm_name = "kandidat49-CompaniesDetected-High"
+
     comparison_operator = "GreaterThanThreshold"
+
     evaluation_periods = 1
-    metric_name = "Latency"
-    namespace = "SentimentApp"
+    metric_name = "sentiment.analysis.companies.detected"
+    namespace = "kandidat49"
     period = 60
     statistic = "Average"
     threshold = 6
-    alarm_description = "Triggered when latency above 6 sec"
-    alarm_actions
+    alarm_description = "Triggered when too many companies in a single analysis"
+    alarm_actions = [aws_sns_topic.alerts.arn]
 }
 
 resource "aws_cloudwatch_dashboard" "app_dashboard"{
-    dashboard_name = "kandidat49Dash"
+    dashboard_name = "kandidat49-SentimentDashboard"
     dashboard_body = jsonencode({
        widgets = [
            {
-               type = "metric"
-               x = 0
-               y = 0
-               width = 12
-               height = 6
-               properties = {
-                   metrics = [
-                       "SentimentApp", "Latency"],
-               period = 60
-               stat = "Average"
-               title = "Latency Over Time"
+               "type" : "metric",
+               "x" : 0 ,
+               "y" : 0,
+               "width" : 12,
+               "height" : 6,
+               "properties" : {
+                   "title" : "Sentiment analysis duration"
+                   "metrics" : [
+                       "kandidat49", "sentiment.analysis.duration"
+                       ],
+              " period" : 60,
+               "stat" : "Average",
+               "view" : "timeSeries"
+
                }
-               },
+           },
            {
-            type = "metric"
-            x = 12
-            y = 0
-            width = 12
-            height = 6
-            properties = {
-                metrics = [
-                    ["SentimentApp", "ErrorCount"],
-                ]
-                stat = "Sum"
-                period = 60
-                title = "Error Count"
+            "type" : "metric",
+            "x" : 12,
+            "y" : 0,
+            "width" : 12,
+            "height" : 6,
+            "properties" : {
+                "title" : "Companies Detected (Gauge)"
+                "metrics" : [
+                    ["kandidat49", "sentiment.analysis.total"]
+                ],
+                "stat" : "Maximum",
+                "period" : 60,
+                "view" : "singleValue"
               }
 
              }
